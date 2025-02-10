76bdb2662cdb20035a814bfb120d38be0aae503d
bpmryandinh <bdinh@my.okcu.edu>
2025-02-10 15:13:07 -0600

    comments
    moved some comments in sepia filter

--- src/main/java/edu/okcu/imagefx/filters/SepiaFilter.java
+++ src/main/java/edu/okcu/imagefx/filters/SepiaFilter.java
@@ -26,8 +26,7 @@ public class SepiaFilter implements IFilter {
                 var newRed = (red * 0.393) + (green * 0.769) + (blue * 0.189);
                 var newGreen =  (red * 0.349) + (green * 0.686) + (blue * 0.168);
                 var newBlue =  (red * 0.272) + (green * 0.534) + (blue * 0.131);
-
-                // Create an Integer for the new values
+                
                //set rgb to 255
                 if (newRed > 255){
                     newRed = 255;
@@ -38,6 +37,8 @@ public class SepiaFilter implements IFilter {
                 if (newBlue > 255){
                     newBlue = 255;
                 }
+
+                // Create an Integer for the new values
                 int newPixel =  ((alpha<<24) | ((int)newRed<<16) | ((int)newGreen<<8) | (int)newBlue);
                 img.setRGB(x, y, newPixel);
             }
@@ -73,6 +74,7 @@ public class SepiaFilter implements IFilter {
                 if (newBlue > 255){
                     newBlue = 255;
                 }
+
                 // Create an Integer for the new values
                 int newPixel =  ((alpha<<24) | ((int)newRed<<16) | ((int)newGreen<<8) | (int)newBlue);
                 img.setRGB(x, y, newPixel);
