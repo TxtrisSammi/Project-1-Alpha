f7c49d463bc48f35723ce22c1925ed9c89471ba8
Samantha Morales <120693377+TxtrisSammi@users.noreply.github.com>
2025-02-10 17:07:53 -0600

    Rotating opposite quadrants
    as opposed to only the top half of the image

--- src/main/java/edu/okcu/imagefx/ImageFXController.java
+++ src/main/java/edu/okcu/imagefx/ImageFXController.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
--- src/main/java/edu/okcu/imagefx/filters/RotationFilter.java
+++ src/main/java/edu/okcu/imagefx/filters/RotationFilter.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class RotationFilter implements IFilter {
-    @Override
+    //@Override
     public BufferedImage apply(BufferedImage img) {
         for (int y = 0; y < img.getHeight(); y++) {
             for (int x = 0; x < img.getWidth(); x++) {
@@ -25,19 +25,37 @@ public class RotationFilter implements IFilter {
 
                 // Create an Integer for the new values
                 int newPixel = (alpha<<24) | (red<<16) | (green<<8) | blue;
-                img.setRGB(y, x, newPixel);
+
+                int newHeight = y - 950;
+                int newWidth = x - 950;
+
+                newHeight -= (newHeight * 2);
+                newWidth -= (newWidth * 2);
+
+                newHeight += 950;
+
+                newWidth += 950;
+
+                img.setRGB(newWidth, newHeight, newPixel);
+
             }
         }
         return img;
     }
 
+
     public javafx.scene.image.Image apply(File file) throws IOException {
         BufferedImage img = ImageIO.read(file);
 
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
+        double halfHeight = img.getHeight() / 2.0;
+        double halfWidth = img.getWidth() / 2.0;
+
+        for (int y = 0; y < halfHeight; y++) {
+            for (int x = 0; x < halfWidth; x++) {
                 int pixel = img.getRGB(x, y);
+
                 Color color = new Color(pixel);
+
 
                 int alpha = color.getAlpha();
                 int red = color.getRed();
@@ -46,11 +64,41 @@ public class RotationFilter implements IFilter {
 
                 int newPixel = (alpha << 24) | (red << 16) | (green << 8) | blue;
 
-                if(x >= y) {
-                    img.setRGB(x, y, newPixel);
-                } else
-                    img.setRGB(y, x, newPixel);
+                double newy = y - halfHeight;
+                double newx = x - halfWidth;
+
+                newy = (-newy);
+                newx = (-newx);
+
+                newy += (halfHeight - 1);
+
+                newx += (halfWidth - 1);
+
+
+
+
+                int pixel2 = img.getRGB((int)newx, (int)newy);
+
+                Color color2 = new Color(pixel2);
+
+
+                int alpha2 = color2.getAlpha();
+                int red2 = color2.getRed();
+                int green2 = color2.getGreen();
+                int blue2 = color2.getBlue();
+
+                int newPixel2 = (alpha2 << 24) | (red2 << 16) | (green2 << 8) | blue2;
+
+
+
+
+
+                img.setRGB(x, y, newPixel2);
+
+                img.setRGB((int)newx, (int)newy, newPixel);
+
             }
+
         }
 
         Image image = ImageUtil.convertBufferedImageToFxImage(img);
